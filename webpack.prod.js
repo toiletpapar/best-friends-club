fc6f09cfaebc34bcd3c907d679127883953d2a39
Tyler Poon <tylerbill99@hotmail.com>
2019-04-27 13:53:39 -0400

    add data

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const webpack = require('webpack')
 
 module.exports = merge(common, {
   mode: 'production',
+  devtool: 'cheap-module-source-map',
   plugins: [
     new webpack.HashedModuleIdsPlugin()
   ]
