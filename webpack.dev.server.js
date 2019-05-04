db0988f1dc6fd3d6e76b10da07d023bca033119b
Tyler Poon <tylerbill99@hotmail.com>
2019-05-04 14:26:43 -0400

    don't emit a bajillion hot update files due to webpack watch

--- webpack.dev.server.js
+++ webpack.dev.server.js
@@ -4,6 +4,10 @@ const webpack = require('webpack')
 
 module.exports = merge.smart(common, {
   mode: 'development',
+  output: {
+    hotUpdateChunkFilename: 'hot/hot-update.js',
+    hotUpdateMainFilename: 'hot/hot-update.json'
+  },
   plugins: [
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin()
