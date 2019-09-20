1294c04a7cf9c5728da9c553ac1abff33210d2a4
Tyler Poon <tylerbill99@hotmail.com>
2019-09-19 22:11:12 -0400

    add message when no games in games list

---
+++ client/Lobby/GameIDList.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const List = styled('ul')`
+  list-style-type: none;
+  border: 1px solid black;
+  height: 400px;
+  border-radius: 5px;
+  overflow-y: auto;
+  padding: 5px;
+`
+
+interface Highlight {
+  active?: boolean;
+}
+
+const GameID = styled('li')<Highlight>`
+  ${(props): string => props.active && `background-color: ${props.theme.darkgray}`}
+`
+
+interface GameIDListProps {
+  gameIDs: string[]
+  onGameIDSelected: (gameID: string) => void 
+  selectedGameID?: string
+}
+
+const GameIDList = (props: GameIDListProps) => {
+  return (
+    <List>
+      {
+        props.gameIDs.length > 0 ? (
+          props.gameIDs.map((gameID): React.ReactNode => {
+            return (
+              <GameID key={gameID} active={props.selectedGameID === gameID} onClick={(): void => props.onGameIDSelected(gameID)}>{gameID}</GameID>
+            )
+          })
+        ) : (
+          <GameID>Unable to find any games. Why not create one?</GameID>
+        )
+      }
+    </List>
+  )
+}
+
+export {
+  GameIDList,
+}
--- client/Lobby/Lobby.tsx
+++ client/Lobby/Lobby.tsx
@@ -1,26 +1,8 @@
 import * as React from 'react'
-import styled from 'styled-components'
 import * as agent from 'superagent'
 
 import { LinkButton, Button } from '../common/index'
-
-interface Highlight {
-  active?: boolean;
-}
-
-const GameIDList = styled('ul')`
-  list-style-type: none;
-  border: 1px solid black;
-  min-height: 50px;
-  max-height: 400px;
-  border-radius: 5px;
-  overflow-y: auto;
-  padding: 0;
-`
-
-const GameID = styled('li')<Highlight>`
-  ${(props): string => props.active && `background-color: ${props.theme.darkgray}`}
-`
+import { GameIDList } from './GameIDList'
 
 const createGame = (
   gameIDs: string[],
@@ -44,15 +26,11 @@ const Lobby = (): JSX.Element => {
     <React.Fragment>
       <h3>Codenames</h3>
       <p>Choose a game from the list below or create your own.</p>
-      <GameIDList>
-        {
-          gameIDs.map((gameID): React.ReactNode => {
-            return (
-              <GameID key={gameID} active={selectedGameID === gameID} onClick={(): void => setSelectedGameID(gameID)}>{gameID}</GameID>
-            )
-          })
-        }
-      </GameIDList>
+      <GameIDList
+        gameIDs={gameIDs}
+        onGameIDSelected={setSelectedGameID}
+        selectedGameID={selectedGameID}
+      />
       <Button left onClick={createGame(gameIDs, setGameID)}>Create Game</Button>
       <LinkButton to={`/codenames/${selectedGameID}`}>Join Game</LinkButton>
     </React.Fragment>
